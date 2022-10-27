80570a1ea46d4e8693e72fea9a0f91c54ef77c9f
SergeiK99 <skx2013@mail.ru>
2022-10-27 18:25:27 +0700

    Initial commit

---
+++ Brush.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3._2
+{
+    abstract class Brush
+    {
+        public Color BrushColor { get; set; }
+        public int Size { get; set; }
+        public Brush(Color brushColor, int size)
+        {
+            BrushColor = brushColor;
+            Size = size;
+        }
+        public abstract void Draw(Bitmap image, int x, int y);
+    }
+    class QuadBrush : Brush
+    {
+        public QuadBrush(Color brushColor, int size) : base(brushColor, size)
+        {
+        }
+
+        public override void Draw(Bitmap image, int x, int y)
+        {
+            for (int y0 = y - Size; y0 < y + Size; y0++)
+            {
+                for (int x0 = x - Size; x0 < x + Size; x0++)
+                {
+                    image.SetPixel(x0, y0, BrushColor);
+                }
+            }
+        }
+    }
+    class Circle : Brush
+    {
+        public Circle(Color brushColor, int size) : base(brushColor, size)
+        {
+        }
+
+        public override void Draw(Bitmap image, int x, int y)
+        {
+           
+        }
+        
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB3._2
+{
+    public partial class Form1 : Form
+    {
+        int _x;
+        int _y;
+        bool _mouseClicked = false;
+        Color SelectedColor
+        {
+            get { return Color.Red; }
+        }
+        int SelectedSize
+        {
+             get { return trackBar1.Value; }
+        }
+        Brush _selecredBrush;
+        Color DefaultColor
+        {
+            get { return Color.White; }
+        }
+        void CreateBlank (int width, int height)
+        {
+            var OldImage = pictureBox1.Image;
+            var bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j<height;j++ )
+                {
+                    bmp.SetPixel(i, j, DefaultColor);
+                }
+            }
+            pictureBox1.Image = bmp;
+            if (OldImage!=null)
+            {
+                OldImage.Dispose();
+            }
+        }
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _selecredBrush = new QuadBrush(SelectedColor, SelectedSize);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_selecredBrush == null)
+            {
+                return;
+            }
+            _selecredBrush.Draw(pictureBox1.Image as Bitmap, _x, _y);
+            pictureBox1.Refresh();
+            _mouseClicked = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _mouseClicked = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            _x = e.X > 0 ? e.X : 0;
+            _y = e.Y > 0 ? e.Y : 0;
+            if (_mouseClicked)
+            {
+                _selecredBrush.Draw(pictureBox1.Image as Bitmap, _x, _y);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _selecredBrush = new Circle(SelectedColor, SelectedSize);
+        }
+
+        private void создатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateBlank(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image=null;
+        }
+    }
+}
